4fc789ad485184549923b0f1359e18024de92dce
Amitya31 <amitya1576@gmail.com>
2026-03-02 20:16:31 +0530

    Temporary remove bullmq to isolate deploy issue

--- lib/queue.ts
+++ lib/queue.ts
@@ -1,25 +1,5 @@
-import {Queue} from "bullmq";
-
-const redisuri = process.env.REDIS_URL;
-
-if(!redisuri) {
-    throw new Error("REDIS_URL is not defined in environment variables");
-}
-
-
-//create a queue for agent tasks
-export const agentQueue = new Queue("AgentJobs",{
-    connection: {
-    url: process.env.REDIS_URL,
+export const agentQueue = {
+  add: async () => {
+    console.log("Job would be added here");
   },
-    defaultJobOptions:{
-        attempts:3,
-        backoff:{
-            type:"exponential",
-            delay:5000
-        },
-        removeOnComplete:100,
-        removeOnFail:1000,
-    }
-})
-
+};
